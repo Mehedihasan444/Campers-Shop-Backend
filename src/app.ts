718d2697fccc9b83bb457467db2e9bef7c31f86a
Mehedi Hasan <mehedihasan67705251@gmail.com>
2024-07-09 11:55:12 +0600

    initial project setup

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Request, Response, Application } from "express";
+import router from "./app/routes";
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ["http://localhost:5173"] }));
+
+// application routes
+app.use("/api", router);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Campers shop is running");
+});
+// app.use(globalErrorHandler);
+
+//Not Found
+// app.use(notFound);
+export default app;
